7813fe0b87e5c1de9d3915a68a31ecf481fa3278
Michael Jiang <mjiang@lcs.on.ca>
2024-11-26 14:15:30 -0500

    Initial Commit

---
+++ ShapeProtocal/ShapeProtocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeProtocalApp.swift
+//  ShapeProtocal
+//
+//  Created by Michael on 2024/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ShapeProtocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
